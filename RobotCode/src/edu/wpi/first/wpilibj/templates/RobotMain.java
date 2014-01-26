4e38984c2bb92af45bab023d57f0f1215defce61
Tanner Kilian <tkilian12@gmail.com>
2014-01-25 23:30:51 -0700

    Added mainDrive code

--- RobotCode/src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ RobotCode/src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -9,42 +9,53 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class RobotMain extends IterativeRobot {
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    //setup left and right drives and assign to pinouts
+    Victor leftDrive = new Victor(1);
+    Victor rightDrive = new Victor(2);
+    
+    //setup controllers
+    Joystick drive1 = new Joystick(1);
+    Joystick drive2 = new Joystick(2);
+    
+    //setup main drive object and point to victors
+    RobotDrive mainDrive = new RobotDrive(leftDrive, rightDrive);
+    
     public void robotInit() {
-
+        
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+        
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        //tankDrive function is called, and assigned joystick inputs
+        mainDrive.tankDrive(drive1.getY(), drive2.getY());
         
+        //arcade drive code commented out, for future reference
+        //mainDrive.arcadeDrive(drive1);
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+        
     }
     
 }
